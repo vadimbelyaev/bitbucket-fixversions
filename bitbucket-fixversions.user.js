f68c517eeebdb44b181fccacd92e70cdfc56738e
Vadim Belyaev <vadimbelyaev@gmail.com>
2020-10-28 10:19:06 +0300

    Increase font size from x-small to small. Remove unnecessary console.log

--- bitbucket-fixversions.user.js
+++ bitbucket-fixversions.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Fix Versions for Bitbucket Pull Requests
 // @namespace    https://github.com/vadimbelyaev/bitbucket-fixversions 
-// @version      0.1
+// @version      0.2
 // @downloadURL  https://github.com/vadimbelyaev/bitbucket-fixversions/raw/main/bitbucket-fixversions.user.js 
 // @supportURL   https://github.com/vadimbelyaev/bitbucket-fixversions/issues
 // @description  Displays fix versions of Jira issues right in the pull request title. The pull request title must contain the Jira issue number(s) and the Bitbucket instance must have the integration with Jira enabled.
@@ -19,12 +19,11 @@
         bbprfixversion_issueLinks.each(function() {
             var bbprfixversion_link = AJS.$(this)
             var bbprfixversion_issueKey = bbprfixversion_link.attr("data-issue-keys");
-            console.log(bbprfixversion_issueKey);
             AJS.$.ajax({
                 url: "/rest/jira-integration/latest/issues?issueKey=" + bbprfixversion_issueKey + "&entityKey=" + bbprfixversion_projectKey + "&fields=fixVersions,-comment&minimum=10",
                 success: function( result ) {
                     var bbprfixversion_fixVersions = result[0].fields.fixVersions.map(fv => fv.name).join(", ");
-                    bbprfixversion_link.after('<sup style="font-size: x-small" title="Fix Version">' + bbprfixversion_fixVersions + '</sup>');
+                    bbprfixversion_link.after('<sup style="font-size: small" title="Fix Version">' + bbprfixversion_fixVersions + '</sup>');
                 }
             });
         });
